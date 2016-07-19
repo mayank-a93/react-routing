dbb99f7f3640ef9e1d7580dcf49796a583fc8806
Mayank Agarwal <mayank.agarwal@cronj.com>
2016-07-19 17:47:38 +0530

    Removed hash key

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Routes = require('./Routes.jsx');
+
+const main = document.getElementById('main');
+ReactDOM.render(Routes, main);
